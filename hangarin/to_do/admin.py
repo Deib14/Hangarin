a0821572a4b1f3ed44d10260fba1ab0410d7aa93
Deib14 <disguiseddev1@gmail.com>
2025-09-04 09:56:38 +0800

    models and admin working properly

--- hangarin/to_do/admin.py
+++ hangarin/to_do/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Priority, Task, SubTask, Note
+
+admin.site.register(Category)
+admin.site.register(Priority)
+admin.site.register(Task)
+admin.site.register(SubTask)
+admin.site.register(Note)
