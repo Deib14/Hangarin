56df126596b710ec3d688744ee3ab51a52515cf5
Deib14 <disguiseddev1@gmail.com>
2025-10-16 06:45:53 +0800

    fix

--- hangarin/hangarin/settings.py
+++ hangarin/hangarin/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import socket
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -47,7 +48,11 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.github',
 ]
-SITE_ID = 4
+
+if "pythonanywhere" in socket.gethostname():
+    SITE_ID = 5
+else:
+    SITE_ID = 4 # local site (127.0.0.1:8000)
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
